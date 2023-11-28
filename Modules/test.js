1c6f98102dea2946231d9eb40c54a8bd4957f15e
rozimuhammad8 <138822332+rozimuhammad8@users.noreply.github.com>
2023-11-28 18:18:41 +0500

    Update test.js

--- Modules/test.js
+++ Modules/test.js
@@ -6,7 +6,7 @@ const test = express.Router();
 const upload = multer().none();
 
 test.get("/test/", upload, (req, res) => {
-  return res.status(200).json([req.ip,req.clientIp]);
+  return res.status(200).json([req.ip,req.clientIp,req.connection.remoteAddress]);
 });
 
 module.exports = test;
